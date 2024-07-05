1b8a5548be2e43a77c0a0e43c78a7010edcf898b
Mark <kazhdan.mark@gmail.com>
2024-07-05 13:49:14 +0300

    Added zero dividing condition at divide method

--- src/main/java/telran/introduction/Calculations.java
+++ src/main/java/telran/introduction/Calculations.java
@@ -10,7 +10,10 @@ public static int multiply(int op1, int op2){
     return res;
 }
 public static int divide (int op1, int op2){
-    int res = op1 / op2;
+    Integer res = null;
+    if (op2 != 0) {
+        res = op1 / op2;
+    }
     return res;
 }
 public static int substract (int op1, int op2){
